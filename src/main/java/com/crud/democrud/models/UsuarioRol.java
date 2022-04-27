cabb3b24d6724d2596f5e9e9cb8309614bc468a6
jmedinr <juandiego619@gmail.com>
2022-04-27 16:41:03 -0500

    ADD --- Se agrega usuariRolRepository a repositories

--- src/main/java/com/crud/democrud/models/UsuarioRol.java
+++ src/main/java/com/crud/democrud/models/UsuarioRol.java
@@ -36,5 +36,5 @@ public class UsuarioRol {
         this.rol = rol;
     }
 
-    
+
 }
---
+++ src/main/java/com/crud/democrud/repositories/UsuarioRolRepository.java
@@ -0,0 +1,13 @@
+package com.crud.democrud.repositories;
+
+import com.crud.democrud.models.UsuarioModel;
+import com.crud.democrud.models.UsuarioRol;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+
+@Repository
+public interface UsuarioRolRepository extends CrudRepository<UsuarioRol, Long> {
+    ArrayList<UsuarioModel> findByRol(String rol);
+}
